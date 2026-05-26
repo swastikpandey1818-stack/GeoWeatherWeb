5bd58775f1fa69f8a7d794dad6eb76c818a3bf50
swastikpandey1818-stack <swastikpandey1818@gmail.com>
2026-05-26 13:22:41 +0530

    update

--- geoapp.py
+++ geoapp.py
@@ -170,6 +170,9 @@ with tab2:
     st.markdown("<h2 style='color:#FFD700;'>🔮 7-Day Regional Extended Forecast</h2>", unsafe_allow_html=True)
     st.markdown("---")
     # Force an initial fetch if it's the very first time the app is opened
+    if st.session_state.w_data is None:
+               fetch_weather_data("Gorakhpur")
+               st.rerun()
     # This forces the app to refresh and populate the data immediately
     if st.session_state.w_data is not None and "daily" in st.session_state.w_data:
         try:
@@ -204,9 +207,7 @@ with tab2:
             st.error(f"Technical Data Error: {e}")
             st.write("Raw data sample:", st.session_state.w_data.keys())
     else:
-        if st.session_state.w_data is None:
-               fetch_weather_data("Gorakhpur")
-               st.rerun()
+      
         st.info("🔍 Loading weather data... please ensure a location is selected.,default Gorakhpur")
 
 with tab3:
